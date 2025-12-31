6316695c1d1ce0f344935e99ac1629a0a7b188f1
Bernardo Soriano <bersoriano@me.com>
2025-12-30 18:52:04 -0600

    updated car brands

--- carBrands.tsx
+++ carBrands.tsx
@@ -1,203 +1,556 @@
 export const carBrandsWithSubModels = [
+  // --- A ---
   {
     name: "Acura",
     abbr: "Acura",
-    subModels: ["ILX", "TLX", "RLX", "MDX", "RDX", "NSX"]
+    subModels: ["ILX", "Integra", "MDX", "NSX", "RDX", "RLX", "TLX"]
   },
   {
     name: "Audi",
     abbr: "Audi",
-    subModels: ["A3", "A4", "A5", "A6", "A7", "A8", "Q3", "Q5", "Q7", "Q8", "TT", "R8", "e-tron"]
+    subModels: ["A3", "A4", "A5", "A6", "A7", "A8", "e-tron", "Q3", "Q5", "Q7", "Q8", "R8", "TT"]
+  },
+  // --- B ---
+  {
+    name: "BAIC",
+    abbr: "BAIC",
+    subModels: ["BJ40", "BJ40 Plus", "D20", "EU5", "Senova X25", "Senova X35", "Senova X55", "U5"]
   },
   {
     name: "BMW",
     abbr: "BMW",
-    subModels: ["1 Series", "2 Series", "3 Series", "4 Series", "5 Series", "7 Series", "X1", "X3", "X5", "X7", "Z4", "i3", "i4", "i7"]
+    subModels: ["1 Series", "2 Series", "3 Series", "4 Series", "5 Series", "7 Series", "i3", "i4", "i7", "iX", "X1", "X3", "X5", "X7", "Z4"]
+  },
+  {
+    name: "Buick",
+    abbr: "Buick",
+    subModels: ["Enclave", "Encore", "Encore GX", "Envision"]
+  },
+  {
+    name: "BYD",
+    abbr: "BYD",
+    subModels: ["Dolphin", "Dolphin Mini", "Han", "King", "Seal", "Seagull", "Shark", "Song Plus", "Tang", "Yuan Plus"]
+  },
+  // --- C ---
+  {
+    name: "Cadillac",
+    abbr: "Cadillac",
+    subModels: ["CT4", "CT5", "Escalade", "Lyriq", "XT4", "XT5", "XT6"]
+  },
+  {
+    name: "Changan",
+    abbr: "Changan",
+    subModels: ["Alsvin", "CS35", "CS55", "CS75", "Eado", "Hunter", "UNI-K", "UNI-T", "UNI-V"]
+  },
+  {
+    name: "Chevrolet",
+    abbr: "Chevy",
+    subModels: [
+      "Aveo",
+      "Beat",
+      "Blazer",
+      "Camaro",
+      "Captiva",
+      "Cheyenne",
+      "Chevy",
+      "Corvette",
+      "Equinox",
+      "Express",
+      "Impala",
+      "Malibu",
+      "Onix",
+      "Silverado",
+      "Spark",
+      "Suburban",
+      "Tahoe",
+      "Tornado",
+      "Tracker",
+      "Traverse",
+      "Trax"
+    ]
+  },
+  {
+    name: "Chirey",
+    abbr: "Chirey",
+    subModels: ["Arrizo 8", "Tiggo 2 Pro", "Tiggo 4 Pro", "Tiggo 7 Pro", "Tiggo 8 Pro", "Tiggo 8 Pro Max"]
+  },
+  {
+    name: "Chrysler",
+    abbr: "Chrysler",
+    subModels: ["300", "Pacifica", "Voyager"]
+  },
+  {
+    name: "Cupra",
+    abbr: "Cupra",
+    subModels: ["Ateca", "Formentor", "Leon"]
+  },
+  // --- D ---
+  {
+    name: "Datsun",
+    abbr: "Datsun",
+    subModels: ["1200", "160J", "180J", "210", "510", "610", "620", "720", "B210", "Bluebird", "Pickup", "Sunny"]
+  },
+  {
+    name: "DFSK",
+    abbr: "DFSK",
+    subModels: ["500", "580", "600", "E5", "EC35", "Glory 560"]
+  },
+  {
+    name: "DINA",
+    abbr: "DINA",
+    subModels: ["Linner", "Runner"]
+  },
+  {
+    name: "Dodge",
+    abbr: "Dodge",
+    subModels: ["Attitude", "Challenger", "Charger", "Durango", "Hornet", "Journey", "Neon", "Vision"]
+  },
+  // --- F ---
+  {
+    name: "Fiat",
+    abbr: "Fiat",
+    subModels: ["500", "500L", "500X", "Ducato", "Fiorino", "Mobi", "Palio", "Palio Adventure", "Pulse", "Strada"]
+  },
+  {
+    name: "Ford",
+    abbr: "Ford",
+    subModels: [
+      "Bronco",
+      "Bronco Sport",
+      "EcoSport",
+      "Edge",
+      "Escape",
+      "Expedition",
+      "Explorer",
+      "F-150",
+      "F-250",
+      "F-350",
+      "F-450",
+      "Fiesta",
+      "Focus",
+      "Fusion",
+      "Lobo",
+      "Maverick",
+      "Mustang",
+      "Ranger",
+      "Transit",
+      "Transit Connect"
+    ]
+  },
+  {
+    name: "Foton",
+    abbr: "Foton",
+    subModels: ["Aumark", "Auman", "Gratour", "Sauvana", "Toano", "Tunland", "View"]
+  },
+  {
+    name: "Freightliner",
+    abbr: "Freightliner",
+    subModels: ["108SD", "114SD", "Cascadia", "M2 106", "M2 112", "PT126"]
+  },
+  // --- G ---
+  {
+    name: "GAC",
+    abbr: "GAC",
+    subModels: ["Emkoo", "Emzoom", "GS3", "GS4", "GS8"]
+  },
+  {
+    name: "Geely",
+    abbr: "Geely",
+    subModels: ["Coolray", "Emgrand", "Geometry C", "GX3 Pro", "Monjaro", "Okavango", "Starray", "Tugella"]
+  },
+  {
+    name: "GMC",
+    abbr: "GMC",
+    subModels: ["Acadia", "Canyon", "Hummer EV", "Savana", "Sierra", "Terrain", "Yukon"]
+  },
+  {
+    name: "Great Wall Motor",
+    abbr: "GWM",
+    subModels: ["Haval H6", "Haval Jolion", "Ora 03", "Ora 07", "Poer"]
+  },
+  // --- H ---
+  {
+    name: "Hino",
+    abbr: "Hino",
+    subModels: ["195", "238", "258", "268", "300 Series", "338", "500 Series", "616", "716", "816"]
+  },
+  {
+    name: "Honda",
+    abbr: "Honda",
+    subModels: [
+      "Accord",
+      "BR-V",
+      "City",
+      "Civic",
+      "CR-V",
+      "Fit",
+      "HR-V",
+      "Insight",
+      "Odyssey",
+      "Passport",
+      "Pilot",
+      "Ridgeline"
+    ]
+  },
+  {
+    name: "Hyundai",
+    abbr: "Hyundai",
+    subModels: [
+      "Accent",
+      "Atos",
+      "Creta",
+      "Elantra",
+      "Grand i10",
+      "H100",
+      "i10",
+      "i20",
+      "IONIQ",
+      "IONIQ 5",
+      "Kona",
+      "Palisade",
+      "Santa Fe",
+      "Sonata",
+      "Starex",
+      "Tucson",
+      "Venue"
+    ]
+  },
+  // --- I ---
+  {
+    name: "Infiniti",
+    abbr: "Infiniti",
+    subModels: ["Q50", "Q60", "QX50", "QX55", "QX60", "QX80"]
+  },
+  {
+    name: "International",
+    abbr: "International",
+    subModels: [
+      "3000RE",
+      "4300",
+      "4700",
+      "CV Series",
+      "DuraStar",
+      "HV Series",
+      "HX Series",
+      "LoneStar",
+      "LT Series",
+      "MV Series",
+      "ProStar",
+      "RH Series",
+      "TerraStar",
+      "WorkStar"
+    ]
+  },
+  {
+    name: "Isuzu",
+    abbr: "Isuzu",
+    subModels: ["D-Max", "ELF", "FRR", "FTR", "FVR", "NPR", "NQR", "NRR"]
+  },
+  // --- J ---
+  {
+    name: "JAC",
+    abbr: "JAC",
+    subModels: [
+      "E10X",
+      "E J7",
+      "E Sei 4 Pro",
+      "Frison T6",
+      "Frison T8",
+      "J2",
+      "J3",
+      "J4",
+      "J7",
+      "Sei2",
+      "Sei4",
+      "Sei4 Pro",
+      "Sei6 Pro",
+      "Sei7 Pro",
+      "Sunray",
+      "T6",
+      "T8"
+    ]
+  },
+  {
+    name: "Jaecoo",
+    abbr: "Jaecoo",
+    subModels: ["J7", "J8"]
+  },
+  {
+    name: "Jeep",
+    abbr: "Jeep",
+    subModels: ["Cherokee", "Compass", "Gladiator", "Grand Cherokee", "Grand Wagoneer", "Renegade", "Wagoneer", "Wrangler"]
+  },
+  {
+    name: "Jetour",
+    abbr: "Jetour",
+    subModels: ["Dashing", "T2", "X70", "X70 Plus", "X90 Plus"]
+  },
+  {
+    name: "JMC",
+    abbr: "JMC",
+    subModels: ["Boarding", "Grand Avenue", "Vigus"]
+  },
+  // --- K ---
+  {
+    name: "Kenworth",
+    abbr: "Kenworth",
+    subModels: ["C500", "K270", "K370", "KW45", "KW55", "T170", "T270", "T370", "T380", "T440", "T460", "T480", "T680", "T800", "T880", "W900", "W990"]
+  },
+  {
+    name: "Kia",
+    abbr: "Kia",
+    subModels: ["Carnival", "EV6", "EV9", "Forte", "K3", "K4", "K5", "Niro", "Rio", "Sedona", "Seltos", "Sorento", "Soul", "Sportage", "Stinger", "Telluride"]
+  },
+  // --- L ---
+  {
+    name: "Lexus",
+    abbr: "Lexus",
+    subModels: ["ES", "GX", "IS", "LC", "LS", "LX", "NX", "RC", "RX", "RZ", "UX"]
+  },
+  {
+    name: "Lincoln",
+    abbr: "Lincoln",
+    subModels: ["Aviator", "Corsair", "MKZ", "Nautilus", "Navigator"]
+  },
+  // --- M ---
+  {
+    name: "Mack",
+    abbr: "Mack",
+    subModels: ["Anthem", "Granite", "LR", "MD", "Pinnacle", "TerraPro"]
+  },
+  {
+    name: "MAN",
+    abbr: "MAN",
+    subModels: ["Lion's City", "TGE", "TGL", "TGM", "TGS", "TGX"]
+  },
+  {
+    name: "Mazda",
+    abbr: "Mazda",
+    subModels: ["CX-3", "CX-30", "CX-5", "CX-50", "CX-70", "CX-9", "CX-90", "Mazda2", "Mazda3", "Mazda6", "MX-5"]
   },
   {
     name: "Mercedes-Benz",
     abbr: "MB",
     subModels: [
       "A-Class",
+      "Actros",
+      "Atego",
       "C-Class",
-      "E-Class",
-      "S-Class",
       "CLA",
       "CLS",
+      "E-Class",
+      "EQB",
+      "EQC",
+      "EQE",
+      "EQS",
+      "G-Class",
       "GLA",
       "GLB",
       "GLC",
       "GLE",
       "GLS",
-      "G-Class",
-      "Sprinter",
-      "Vito",
       "Metris",
-      "EQB",
-      "EQC",
-      "EQS"
+      "O500",
+      "OF",
+      "S-Class",
+      "Sprinter",
+      "Torino",
+      "Vito"
     ]
   },
+  {
+    name: "MG",
+    abbr: "MG",
+    subModels: ["3", "4", "5", "eHS", "GT", "HS", "Marvel R", "One", "RX5", "RX8", "ZS", "ZS EV"]
+  },
+  {
+    name: "Mini",
+    abbr: "Mini",
+    subModels: ["Clubman", "Convertible", "Cooper", "Countryman", "Hardtop 2 Door", "Hardtop 4 Door"]
+  },
+  {
+    name: "Mitsubishi",
+    abbr: "Mitsubishi",
+    subModels: ["ASX", "Eclipse Cross", "L200", "Mirage", "Montero", "Outlander", "Outlander Sport", "Xpander"]
+  },
+  // --- N ---
   {
     name: "Nissan",
     abbr: "Nissan",
     subModels: [
-      "March",
-      "Versa",
-      "Sentra",
+      "350Z",
+      "370Z",
       "Altima",
-      "Maxima",
-      "Leaf",
-      "Kicks",
-      "Juke",
-      "Rogue",
-      "X-Trail",
-      "Pathfinder",
-      "Murano",
       "Armada",
+      "Frontier",
+      "GT-R",
+      "Juke",
+      "Kicks",
+      "Leaf",
+      "March",
+      "Maxima",
+      "Murano",
+      "NP300",
       "NV200",
+      "NV350",
+      "Pathfinder",
+      "Platina",
+      "Rogue",
+      "Sentra",
+      "Tiida",
+      "Titan",
+      "Tsuru",
+      "Tsubame",
       "Urvan",
-      "Frontier"
+      "V-Drive",
+      "Versa",
+      "X-Trail"
     ]
   },
+  // --- O ---
   {
-    name: "Ford",
-    abbr: "Ford",
-    subModels: [
-      "Fiesta",
-      "Focus",
-      "Fusion",
-      "Mustang",
-      "Bronco",
-      "Escape",
-      "Edge",
-      "Explorer",
-      "Expedition",
-      "Ranger",
-      "F-150",
-      "F-250",
-      "F-350",
-      "Transit",
-      "Transit Connect",
-      "Maverick"
-    ]
+    name: "Omoda",
+    abbr: "Omoda",
+    subModels: ["C5", "C5 EV", "C5 GT", "O5", "O5 GT"]
+  },
+  // --- P ---
+  {
+    name: "Peugeot",
+    abbr: "Peugeot",
+    subModels: ["2008", "208", "3008", "301", "308", "5008", "508", "Boxer", "E-Partner", "E-Rifter", "Expert", "Manager", "Partner", "Partner Tepee", "Rifter"]
+  },
+  // --- R ---
+  {
+    name: "Ram",
+    abbr: "Ram",
+    subModels: ["1500", "2500", "3500", "4000", "700", "ProMaster", "ProMaster City", "ProMaster Rapid"]
+  },
+  {
+    name: "Renault",
+    abbr: "Renault",
+    subModels: ["Arkana", "Captur", "Duster", "Kangoo", "Kardian", "Koleos", "Kwid", "Logan", "Master", "Megane E-Tech", "Oroch", "Sandero", "Stepway", "Twizy"]
+  },
+  // --- S ---
+  {
+    name: "Scania",
+    abbr: "Scania",
+    subModels: ["G Series", "K Series", "L Series", "P Series", "R Series", "S Series"]
+  },
+  {
+    name: "SEAT",
+    abbr: "SEAT",
+    subModels: ["Arona", "Ateca", "Ibiza", "Leon", "Leon ST", "Tarraco"]
+  },
+  {
+    name: "Seres",
+    abbr: "Seres",
+    subModels: ["3", "5 EV"]
+  },
+  {
+    name: "Subaru",
+    abbr: "Subaru",
+    subModels: ["Ascent", "BRZ", "Crosstrek", "Forester", "Impreza", "Legacy", "Outback", "Solterra", "WRX"]
+  },
+  {
+    name: "Suzuki",
+    abbr: "Suzuki",
+    subModels: ["Baleno", "Ciaz", "Ertiga", "Fronx", "Grand Vitara", "Ignis", "Jimny", "S-Cross", "Swift", "Vitara"]
+  },
+  // --- T ---
+  {
+    name: "Tesla",
+    abbr: "Tesla",
+    subModels: ["Cybertruck", "Model 3", "Model S", "Model X", "Model Y"]
   },
   {
     name: "Toyota",
     abbr: "Toyota",
     subModels: [
-      "Yaris",
-      "Corolla",
-      "Camry",
-      "Avalon",
-      "Prius",
-      "GR86",
-      "Supra",
-      "Raize",
-      "Rush",
-      "RAV4",
-      "Highlander",
       "4Runner",
-      "Sequoia",
-      "Land Cruiser",
+      "Avalon",
+      "Avanza",
+      "bZ4X",
+      "Camry",
+      "C-HR",
+      "Corolla",
+      "Corolla Cross",
+      "GR86",
+      "Grand Highlander",
+      "HiAce",
+      "Highlander",
       "Hilux",
+      "Land Cruiser",
+      "Prius",
+      "Raize",
+      "RAV4",
+      "Rush",
+      "Sequoia",
+      "Sienna",
+      "Supra",
       "Tacoma",
       "Tundra",
-      "HiAce"
+      "Yaris"
     ]
   },
+  // --- V ---
   {
     name: "Volkswagen",
     abbr: "VW",
     subModels: [
-      "Gol",
-      "Polo",
-      "Jetta",
-      "Vento",
-      "Passat",
-      "Arteon",
-      "Taos",
-      "Tiguan",
-      "Teramont",
-      "Atlas",
-      "Saveiro",
       "Amarok",
+      "Arteon",
+      "Atlantic",
+      "Atlas",
+      "Beetle",
+      "Bora",
+      "Brasilia",
       "Caddy",
-      "Transporter",
+      "Caravelle",
+      "Combi",
+      "Constellation",
+      "Crafter",
+      "Delivery",
+      "Gol",
+      "Golf",
       "ID.4",
-      "ID.Buzz"
+      "ID.Buzz",
+      "Jetta",
+      "Nivus",
+      "Passat",
+      "Pointer",
+      "Polo",
+      "Safari",
+      "Saveiro",
+      "Sedan",
+      "T-Cross",
+      "Taos",
+      "Teramont",
+      "Tiguan",
+      "Transporter",
+      "Vento",
+      "Virtus",
+      "Vocho"
     ]
   },
   {
-    name: "Foton",
-    abbr: "Foton",
-    subModels: [
-      "Tunland",
-      "Toano",
-      "View",
-      "Aumark",
-      "Gratour"
-    ]
+    name: "Volkswagen Camiones y Buses",
+    abbr: "VW Trucks",
+    subModels: ["Constellation", "Crafter Chassis", "Delivery", "e-Delivery", "MAN TGE", "Worker"]
   },
   {
-    name: "Chevrolet",
-    abbr: "Chevy",
-    subModels: [
-      "Spark",
-      "Aveo",
-      "Onix",
-      "Malibu",
-      "Impala",
-      "Camaro",
-      "Corvette",
-      "Trax",
-      "Tracker",
-      "Equinox",
-      "Blazer",
-      "Suburban",
-      "Tahoe",
-      "Silverado"
-    ]
+    name: "Volvo",
+    abbr: "Volvo",
+    subModels: ["C40", "EX30", "EX90", "S60", "S90", "V60", "V90", "XC40", "XC60", "XC90"]
   },
   {
-    name: "Hyundai",
-    abbr: "Hyundai",
-    subModels: ["i10", "i20", "Elantra", "Sonata", "Accent", "Kona", "Tucson", "Santa Fe", "Palisade", "IONIQ"]
+    name: "Volvo Buses",
+    abbr: "Volvo Buses",
+    subModels: ["7900", "8900", "9700", "9800", "9900", "B8R", "B11R", "B13R"]
   },
   {
-    name: "Kia",
-    abbr: "Kia",
-    subModels: ["Rio", "Forte", "K5", "Stinger", "Soul", "Seltos", "Sportage", "Sorento", "Telluride", "EV6"]
-  },
-  {
-    name: "Mazda",
-    abbr: "Mazda",
-    subModels: ["Mazda2", "Mazda3", "Mazda6", "CX-3", "CX-30", "CX-5", "CX-50", "CX-9", "MX-5"]
-  },
-  {
-    name: "BYD",
-    abbr: "BYD",
-    subModels: ["Dolphin", "Seal", "Han", "Song", "Tang", "Yuan Plus", "Shark"]
-  },
-  {
-    name: "MG Motor",
-    abbr: "MG",
-    subModels: ["MG3", "MG4", "MG5", "MG GT", "ZS", "HS", "RX8"]
-  },
-  {
-    name: "Changan",
-    abbr: "Changan",
-    subModels: ["Alsvin", "Eado", "CS35", "CS55", "CS75", "UNI-T", "UNI-K"]
-  },
-  {
-    name: "JAC",
-    abbr: "JAC",
-    subModels: ["J2", "J3", "J4", "J7", "Sei2", "Sei4", "Sunray"]
-  },
-  {
-    name: "Geely",
-    abbr: "Geely",
-    subModels: ["GX3", "Emgrand", "Coolray", "Tugella", "Geometry C"]
+    name: "Volvo Trucks",
+    abbr: "Volvo Trucks",
+    subModels: ["FH", "FH16", "FM", "FMX", "VNL", "VNR", "VNX"]
   }
 ];
 
